a4e01a3230317573f55fa1b385f3706e972642bf
daviaquino87 <davi26031@gmail.com>
2022-08-12 23:09:25 -0300

    feat(create rote):create route by list spendings

--- src/routes/spending.routes.js
+++ src/routes/spending.routes.js
@@ -1,6 +1,7 @@
 const { application } = require('express');
 const express = require('express');
 const routes = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 routes.use(express.json());
 
@@ -9,14 +10,20 @@ const spendings = []
 routes.post('/spending',(request,response) => {
     const {date,amount} = request.body;
 
-
     const purchase = {
+        id: uuidv4(),
         date:date,
         amount: amount
     }
 
     spendings.push(purchase);
 
+    return response.status(201).send();
+});
+
+routes.get('/spending',(request,response) => {
     return response.status(201).json({spendings:spendings});
 });
+
+
 module.exports = routes;
