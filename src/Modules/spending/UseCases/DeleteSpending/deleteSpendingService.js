cb4f06c70022293433f82734c5af76254610cbf3
daviaquino87 <davi26031@gmail.com>
2022-08-25 10:48:04 -0300

    feat(edit delet):edit function delete spending

--- src/Modules/spending/UseCases/DeleteSpending/deleteSpendingService.js
+++ src/Modules/spending/UseCases/DeleteSpending/deleteSpendingService.js
@@ -2,18 +2,11 @@ const { supabase } = require("../../../../database/Supabase");
 
 class DeleteSpendingService {
   async deleteSpending(id, uuid) {
-    const { data: spending, error } = await supabase
+    const { data, error } = await supabase
       .from("spending")
       .delete()
       .eq("user_id", uuid)
       .match({ id });
-    console.log("error", error);
-    console.log("spending", spendings);
-
-    const data = {
-      spending,
-      error,
-    };
 
     return data;
   }
