fa96283b63ad2d4941b080d525921ad74788ba2d
daviaquino87 <davi26031@gmail.com>
2022-08-25 22:35:24 -0300

    buil(debug):debug aplication

--- src/middleware/autorization.js
+++ src/middleware/autorization.js
@@ -3,17 +3,21 @@ const { jwtDecode } = require("../utils/jwtDecode");
 function Autorization(request, response, next) {
   const token = request.headers;
 
-  if (!token.authorization) {
-    return response.status(401).json({ msg: "usuario não validado" });
+  try {
+    if (!token.authorization) {
+      return response.status(401).json({ msg: "usuario não validado" });
+    }
+
+    const time = jwtDecode(token.authorization).exp;
+
+    if (new Date(time * 1000).getTime() < new Date().getTime()) {
+      return response.status(401).json({ msg: "falha na validação" });
+    }
+    next();
+  } catch (err) {
+    console.log(err);
+    response.status(401).json({ msg: "unauthorized" });
   }
-
-  const time = jwtDecode(token.authorization).exp;
-
-  if (new Date(time * 1000).getTime() < new Date().getTime()) {
-    return response.status(401).json({ msg: "falha na validação" });
-  }
-
-  next();
 }
 
 module.exports = { Autorization };
