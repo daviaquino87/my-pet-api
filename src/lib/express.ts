954b8d208b17aff385bf263ca27ccdd3b69bd797
daviaquino87 <davi26031@gmail.com>
2023-05-05 09:56:22 -0300

    feat: liberação de cors

--- src/lib/express.ts
+++ src/lib/express.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import "express-async-errors";
+import cors from "cors";
 
 import express from "express";
 
@@ -8,6 +9,7 @@ import { validadeError } from "@/shared/http/middleware/validate-error";
 import { verifyRouteNotFound } from "@/shared/http/middleware/verify-route-not-found";
 
 export const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(indexRoutes);
